bb211f63f2d9d6cfdb2e5629b90931681c0b24fe
BogdanYukin <b.yukin2@gmail.com>
2016-11-21 11:20:17 +0300

    +

--- master/styles/default/js/main.js
+++ master/styles/default/js/main.js
@@ -1,1 +1,19 @@
+ymaps.ready(init);
+        var myMap,
+            myPlacemark;
 
+        function init(){
+            myMap = new ymaps.Map("location-map", {
+                center: [55.72355770, 36.20014961],
+                zoom: 9
+            });
+
+            myPlacemark = new ymaps.Placemark([55.71525143, 36.18837718], {
+                hintContent: 'Белая Руза',
+                balloonContent: 'Белая Руза. Котеджный поселок'
+            });
+
+            myMap.geoObjects.add(myPlacemark);
+			myMap.behaviors.disable('scrollZoom');
+			myMap.behaviors.disable('drag');
+        }
